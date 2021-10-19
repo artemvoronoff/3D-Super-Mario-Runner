cde47691ca93ea3bce02b32e31b66a8c3aa19627
artemvoronoff <80431170+artemvoronoff@users.noreply.github.com>
2021-10-19 17:30:41 -0500

    basic files
    -initial files project

---
+++ 3D Super Mario Runner/Assets/Scripts/GameController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public string message = "Hello there!";
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(message);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
